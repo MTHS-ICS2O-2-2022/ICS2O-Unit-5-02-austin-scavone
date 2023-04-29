2362390fb626dc99f68ed93464108b958572c5e0
austin <124389297+austin-scavone@users.noreply.github.com>
2023-04-29 16:27:46 +0000

    changes

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+/**
+ * This function displays the slider value.
+ */
+function myButtonClicked() {
+  alwaysOnButtonChecked = document.getElementById('on-check').checked
+
+  if (alwaysOnButtonChecked == true) {
+    document.getElementById("radio-button-value").innerHTML =
+    "<p>Value is: On</p>"
+  } else {
+    document.getElementById("radio-button-value").innerHTML =
+    "<p>Value is: Off</p>"
+  }
+}
